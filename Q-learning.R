9cdb238f985c5e21ea9093cc0f07f6d8dd9a9c49
Nitin Agarwal <mnitin3@gmail.com>
2020-03-17 23:57:22 +0530

    Update Q-learning.R

--- Q-learning.R
+++ Q-learning.R
@@ -16,7 +16,7 @@ plot_matrix <- function(mat_pot, digits_arg){
   digits_arg <- ifelse(missing(digits_arg),0,digits_arg)
   plot(mat_pot, cex = 1.2, fmt.cell=paste0('%.',digits_arg,'f'), 
        col=brewer.pal(3,"Blues"), breaks=c(-500, 0, 0, 500),
-       xlab="Actions", ylab = "States",main = "")
+       xlab="state", ylab = "",main = "")
 }
 
 ###### Define Environment #######
@@ -32,9 +32,11 @@ rewards <- c(0,-100,-100,0,-100,
              10,10,10,10,-100,
              -100,-100,10,10,100
 )
-goal <- which(rewards==max(rewards), arr.ind=TRUE)
 rewards_mat <- matrix(rewards, nrow = length(states), ncol= length(states))
 plot_matrix(rewards_mat) ## matrix, digits
+
+goal <- which(rewards==max(rewards), arr.ind=TRUE)
+goal
 # > rewards_mat
 #       [,1] [,2] [,3] [,4] [,5]
 # [1,]    0   10   10   10 -100
@@ -57,43 +59,23 @@ getNextStates <- function(cs) {
     ns <- c(cs - 1, 
             cs - stalen, 
             cs + stalen);
-    #,cs - stalen - 1
-    #,cs + stalen - 1);
-    
   } else if (aa$x == min(states)) {
     ns <- c(cs + 1, 
             cs - stalen, 
             cs + stalen);
-    #, cs - stalen + 1);#, 
-    #cs + stalen + 1);
   } else {
     ns <- c(cs + 1,
             cs - 1,  
             cs - stalen, 
             cs + stalen);
-    #,cs + stalen + 1
-    #,cs - stalen + 1
-    #,cs + stalen - 1
-    #,cs - stalen - 1);
   }
   nss <- sort(ns[ns > 0 & ns <= NS]);
   return(nss);
 }
-getNextStates(1)
-
-
-## Get reward for current state
-get_Prereward <- function(current_state){
-  reward_curr <- rewards[current_state]
-  if(reward_curr == 0 | is.na(reward_curr) | length(reward_curr) == 0 ){
-    reward_curr<-0
-  }
-  return(reward_curr)
-}
-
+getNextStates(25)
 
 ########### Episodes Execution ################
-N <- 20         # No. of Episode
+N <- 10        # No. of Episode
 alpha <- 0.8    # Learning Rate
 gamma <- 0.7    # Discount Factor
 
@@ -106,15 +88,16 @@ for (i in 1:N) {
   cat("\nStart Episode: ", current_episode)
   
   ## choose next state from possible actions at current state
-  ## need to create environment. Where not possible actions get -1 in Reward.
   cs <- sample(state_seq$state, 1)
   cat("\n\tCurrent state: ", cs)
-  cs1 <- cs;
   step_num <- 1; 
   while (T) {
-    plot_matrix(Q,1)
-    Sys.sleep(0.2)
-    reward <- get_Prereward(cs)
+    cat("\n\n\tStep no.: ", step_num)
+    cat("\n\t\tCurrent State: ", cs)
+    reward <- rewards[cs]
+    if(reward == 0 | is.na(reward) | length(reward) == 0 ){
+      reward <- 0
+    }
     cat("\n\t\tReward CS: ", reward)
     next.states <- getNextStates(cs);
     cat("\n\t\tPossible next states: ", next.states)
@@ -128,13 +111,15 @@ for (i in 1:N) {
     }
     cat("\n\t\tNext state: ", ns)
     
-    
     # Update Q values for next states.
     Q[cs] <- round(Q[cs] + alpha * (reward + gamma * max(Q[getNextStates(ns)])-Q[cs]),1);
-    cs <- ns;
-    if (cs1 == goal | step_num > 20) {
+    cat("\n\t\tNew Q-Value: ", Q[cs])
+    plot_matrix(Q,1)
+    Sys.sleep(0.2)
+    if (cs == goal | step_num > 20) {
       break;
     }
+    cs <- ns;
     step_num <- step_num + 1;
   }
   cat("\nEnd Episode: ", current_episode)
